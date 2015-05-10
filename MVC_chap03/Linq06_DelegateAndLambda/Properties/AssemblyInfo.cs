3ede4dab448db2038ae2fbce74edd72cad9f37ad
chihkang <chihkang@me.com>
2015-05-10 13:09:07 +0800

    MVC-chap03

---
+++ MVC_chap03/Linq06_DelegateAndLambda/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Linq06_DelegateAndLambda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linq06_DelegateAndLambda")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f702315-fb68-4c00-813f-7440605de7cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MVC_chap03/Linq07_LINQStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq07_LINQStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> list2 = new List<int>() { 6, 4, 2, 7, 9, 0 };
+
+            // join query by statement.
+            var query = from item1 in list1
+                        join item2 in list2 on item1 equals item2
+                        select item2;
+
+            foreach (var q in query.Where((c, c2) => c > 2))
+                Console.Write("{0} ", q);
+
+            Console.WriteLine();
+
+            var query2 = from bigger in list1
+                         select bigger;
+
+            foreach (var q in query2.Where(c=>c>2 && c<6))
+                Console.Write("{0} ",q);
+
+            Console.ReadLine();
+        }
+    }
+}
